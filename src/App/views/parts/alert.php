8c38f01dead521a73fda502707a9e2f277ddaaaf
Esteban <eiarza@msa.com.ar>
2025-09-16 01:03:43 -0300

    Acomodamos el deploy

--- src/App/views/parts/alert.php
+++ src/App/views/parts/alert.php
@@ -1,4 +1,3 @@
-<? var_dump($errors);?>
 <?php if (!empty($messages)): ?>
     <section class="alert alert-<?php echo htmlspecialchars($type, ENT_QUOTES, 'UTF-8'); ?>">
         <button class="alert-close" onclick="this.parentElement.style.display='none';">&times;</button>
